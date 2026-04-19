c21be1e5218a2c0f19d64e6228591266c65b08de
fangnster <fangnster@gmail.com>
2026-04-19 20:42:54 +0800

    feat(infra): pr时前端检查不通过，修复了几个相关前端文件
    因为是页面和postgres联调，所以有少量前端页面的改动

--- apps/web/src/components/wallet/WalletProvider.tsx
+++ apps/web/src/components/wallet/WalletProvider.tsx
@@ -7,8 +7,9 @@ import {
   WalletProvider as SolanaWalletProvider,
 } from '@solana/wallet-adapter-react'
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
-import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare'
 import { clusterApiUrl } from '@solana/web3.js'
 
 interface WalletProviderProps {
